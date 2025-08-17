0bf8d8d68e37edd00eb4fb39607b66e9109147f9
tikrack <tikrackcode@gmail.com>
2025-08-17 16:00:00 +0330

    [Captal caracter]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+fn build_mapping() -> HashMap<usize, char> {
+    let mut map = HashMap::new();
+
+    for (i, c) in ('A'..='Z').enumerate() {
+        map.insert(i + 1, c);
+    }
+
+    map
 }
+
+fn main() {}
